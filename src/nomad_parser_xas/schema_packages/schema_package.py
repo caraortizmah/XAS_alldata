e31f1e93aae88684afc53e29cd878f929249e72b
caraortizmah <caraortizmah@gmail.com>
2024-09-06 19:10:21 +0200

    Class ROCISDFT_xas_state() and ROCISDFT_xas() differentiated. The first one is called in the parser.py

--- src/nomad_parser_xas/schema_packages/schema_package.py
+++ src/nomad_parser_xas/schema_packages/schema_package.py
@@ -13,9 +13,9 @@ if TYPE_CHECKING:
 import numpy as np
 
 from nomad.config import config
-from nomad.datamodel.data import Schema
+from nomad.datamodel.data import Schema, ArchiveSection
 from nomad.datamodel.metainfo.annotations import ELNAnnotation, ELNComponentEnum
-from nomad.metainfo import Quantity as mQuantity, SchemaPackage
+from nomad.metainfo import Quantity as mQuantity, SchemaPackage, SubSection
 from nomad_simulations.schema_packages.properties.spectral_profile import XASSpectrum
 
 configuration = config.get_plugin_entry_point(
@@ -23,6 +23,39 @@ configuration = config.get_plugin_entry_point(
 )
 
 m_package = SchemaPackage()
+
+class ROCISDFT_xas_state(ArchiveSection):
+    occupied = mQuantity(
+        type=np.int32,
+        shape=['*'],
+        description="""
+        Index of the occupied molecular orbital from a resonant transition
+        """,
+    )
+
+    virtual = mQuantity(
+        type=np.int32,
+        shape=['*'],
+        description="""
+        Index of the virtual molecular orbital from a resonant transition
+        """,
+    )
+
+    trans_prob = mQuantity(
+        type=np.float64,
+        shape=['*'],
+        description="""
+        Transition probability of the occupied-virtual molecular orbital coupling
+        """,
+    )
+
+    trans_amp = mQuantity(
+        type=np.float64,
+        shape=['*'],
+        description="""
+        Transition amplitude of the occupied-virtual molecular orbital coupling
+        """,
+    )
 
 
 class ROCISDFT_xas(XASSpectrum):
@@ -36,13 +69,7 @@ class ROCISDFT_xas(XASSpectrum):
         """,
     )
 
-    orca_state = mQuantity(
-        type=np.float64,
-        shape=['*'],
-        description="""
-        Taking the excited states from Eigenvectors of ROCIS calculation:
-        """,
-    )
+    orca_state = SubSection(sub_section=ROCISDFT_xas_state.m_def, repeats=True, description='')
 
 
 m_package.__init_metainfo__()
